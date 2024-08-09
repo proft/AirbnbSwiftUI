96aeff8e219a4241476a5ef68eb2af474d553afe
proft <proftua@gmail.com>
2024-08-09 16:35:08 +0300

    Initial Commit

---
+++ AirbnbSwiftUI/AirbnbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSwiftUIApp.swift
+//  AirbnbSwiftUI
+//
+//  Created by Ivan Morgun on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
